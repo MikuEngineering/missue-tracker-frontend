c9b5492350f1371ea2172d822d459c13c83a9dc6
DevilTea <ch870814@gmail.com>
2019-12-13 17:07:38 +0800

    Add auth route and view

---
+++ src/views/AuthView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>auth</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class AuthView extends Vue {
+}
+</script>
+
+<style>
+</style>
