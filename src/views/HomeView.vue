bc1422e72ee9c9e8e6eb115888f7a5d7475d1db4
DevilTea <ch870814@gmail.com>
2019-12-23 02:25:22 +0800

    Move views in shared layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,17 @@
 <template>
-  <DefaultLayout>
-    <GuestView v-if="isGuest"></GuestView>
-    <UserView v-else></UserView>
-  </DefaultLayout>
+  <GuestView v-if="isGuest"></GuestView>
+  <UserView v-else></UserView>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import GuestView from '@/components/home/GuestView.vue'
 import UserView from '@/components/home/UserView.vue'
 import UserModule from '@/store/modules/user'
 
 @Component({
   components: {
-    DefaultLayout,
     GuestView,
     UserView
   }
