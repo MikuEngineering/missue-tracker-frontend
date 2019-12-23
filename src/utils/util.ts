f4f86da06be7680cd97d172d53b668400099a34d
DevilTea <ch870814@gmail.com>
2019-12-23 21:19:36 +0800

    Add common api error handler

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,11 +1,15 @@
+import { ApiError, BadRequestResponse, OtherClientErrorResponse } from '@/api/Api'
+import ErrorCode from '@/enums/ErrorCode'
+import AppModule from '@/store/modules/app'
+
 export function delay (ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 export async function mockGetProjectIds () {
   await delay(300)
-  return [1, 2, 3, 4, 5, 6, 7]
-  // return []
+  // return [1, 2, 3, 4, 5, 6, 7]
+  return []
 }
 
 export async function mockGetProjectInfo (projectId: number) {
@@ -16,5 +20,19 @@ export async function mockGetProjectInfo (projectId: number) {
     description: `A missue-tracker project-${projectId}.`,
     privacy: Math.floor(Math.random() * 2),
     tags: ['typescript', 'vue', 'scss']
+  }
+}
+
+export function apiErrorHandler (error: Error) {
+  if (!(error instanceof ApiError)) throw error
+
+  const apiError: ApiError = error
+
+  if (apiError.code === ErrorCode.BadRequest) {
+    const data: BadRequestResponse = apiError.data
+    data.errors.forEach(err => AppModule.addAlert({ type: 'error', message: err.message }))
+  } else {
+    const data: OtherClientErrorResponse = apiError.data
+    AppModule.addAlert({ type: 'error', message: data.message })
   }
 }
